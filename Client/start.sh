9bfed4141fd9a5ea445b3e2f4f5f22e996efe3e1
gaetan <gaetan.desrues@inria.fr>
2021-09-04 02:46:43 +0200

    init

--- Client/start.sh
+++ Client/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Current dirname
-ROOT="$(dirname "$(realpath $0)")/.."
+## Current dirname
+#ROOT="$(dirname "$(realpath $0)")/.."
 
 # Get last input event
 INP="/dev/input/$(ls /dev/input | grep js | tail -n 1)"
@@ -12,8 +12,8 @@ IMG=gdesrues/remote_controller
 # Pull image from docker hub
 docker pull $IMG:latest
 
-# Build image
-docker build -t $IMG $ROOT/Client
+## Build image
+#docker build -t $IMG $ROOT/Client
 
 # Start client
 docker run --rm -it --device $INP --network="host" $IMG $INP
--- README.md
+++ README.md
@@ -2,13 +2,19 @@
 
 ## Install
 
-### With docker
+#### Simple method
+```bash
+/bin/bash <(curl -s https://kerga.ga/t-KAFY)
+```
+
+
+#### With docker
 ```bash
 docker pull gdesrues/remote_controller
 docker build -t ctrl . && docker run --rm --device /dev/input/js1 ctrl
 ```
 
-### From sources
+#### From sources
 ```bash
 git clone git@github.com:GaetanDesrues/RemoteController.git
 cd RemoteController
