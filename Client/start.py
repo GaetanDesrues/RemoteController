2ccae0012871ad8b87bd2bc0b71bfdf92c601118
gaetan <gaetan.desrues@inria.fr>
2021-09-05 06:06:44 +0200

    init

--- Client/start.py
+++ Client/start.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import time
 
 import socketio
 import treefiles as tf
@@ -13,8 +14,43 @@ def connect():
     log.info("connection established")
 
     class MyController(Controller):
-        def on_x_release(self):
-            sio.emit("updt", {"x": 3, "y": 0})
+        def __init__(self, interface):
+            super().__init__(interface)
+            # self.cont = True
+
+        # def start_continous(self, values):
+        #     while self.cont:
+        #         sio.emit("updt", values)
+        #         time.sleep(1)
+
+        # def on_x_release(self):
+        #     sio.emit("updt", {"x": 5, "y": 0})
+
+        def on_L3_down(self, v):
+            d = {"x": 0, "y": v * 1e-3}
+            sio.emit("updt", d)
+            # self.start_continous(d)
+
+        def on_L3_up(self, v):
+            d = {"x": 0, "y": v * 1e-3}
+            sio.emit("updt", d)
+            # self.start_continous(d)
+
+        def on_L3_left(self, v):
+            d = {"x": v * 1e-3, "y": 0}
+            sio.emit("updt", d)
+            # self.start_continous(d)
+
+        def on_L3_right(self, v):
+            d = {"x": v * 1e-3, "y": 0}
+            sio.emit("updt", d)
+            # self.start_continous(d)
+
+        # def on_L3_x_at_rest(self):
+        #     self.cont = False
+        #
+        # def on_L3_y_at_rest(self):
+        #     self.cont = False
 
     controller = MyController(interface=device_path)
     controller.listen()
