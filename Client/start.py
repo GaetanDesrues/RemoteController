c57ea7c61f9112d42d40cac19639a192df439051
gaetan <gaetan.desrues@inria.fr>
2021-09-05 06:13:42 +0200

    init

--- Client/start.py
+++ Client/start.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import time
 
 import socketio
 import treefiles as tf
@@ -14,50 +13,37 @@ def connect():
     log.info("connection established")
 
     class MyController(Controller):
-        def __init__(self, interface):
-            super().__init__(interface)
-            # self.cont = True
+        def on_L2_down(self, v):
+            sio.emit("updt", {"x": 0, "y": v * 1e-3})
 
-        # def start_continous(self, values):
-        #     while self.cont:
-        #         sio.emit("updt", values)
-        #         time.sleep(1)
+        def on_L2_up(self, v):
+            sio.emit("updt", {"x": 0, "y": v * 1e-3})
 
-        # def on_x_release(self):
-        #     sio.emit("updt", {"x": 5, "y": 0})
+        def on_L2_left(self, v):
+            sio.emit("updt", {"x": v * 1e-3, "y": 0})
+
+        def on_L2_right(self, v):
+            sio.emit("updt", {"x": v * 1e-3, "y": 0})
 
         def on_L3_down(self, v):
-            d = {"x": 0, "y": v * 1e-3}
-            sio.emit("updt", d)
-            # self.start_continous(d)
+            sio.emit("updt", {"x": 0, "y": v * 1e-3})
 
         def on_L3_up(self, v):
-            d = {"x": 0, "y": v * 1e-3}
-            sio.emit("updt", d)
-            # self.start_continous(d)
+            sio.emit("updt", {"x": 0, "y": v * 1e-3})
 
         def on_L3_left(self, v):
-            d = {"x": v * 1e-3, "y": 0}
-            sio.emit("updt", d)
-            # self.start_continous(d)
+            sio.emit("updt", {"x": v * 1e-3, "y": 0})
 
         def on_L3_right(self, v):
-            d = {"x": v * 1e-3, "y": 0}
-            sio.emit("updt", d)
-            # self.start_continous(d)
-
-        # def on_L3_x_at_rest(self):
-        #     self.cont = False
-        #
-        # def on_L3_y_at_rest(self):
-        #     self.cont = False
+            sio.emit("updt", {"x": v * 1e-3, "y": 0})
 
     controller = MyController(interface=device_path)
     controller.listen()
 
 
 def start_client():
-    url = f"http://127.0.0.1:5011"
+    # url = "http://127.0.0.1:5011"
+    url = "https://loc.kerga.ga"
 
     log.info(f"connecting to {url}")
     sio.connect(url)
